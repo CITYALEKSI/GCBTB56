b2ba523cbe173dd0460da75e020a9739156d69db
CITYALEKSI <turungraffitit@gmail.com>
2026-07-05 19:07:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,30 +5,23 @@
 =====================================
 */
 
-
 const archive = {
     version: "1.4",
     author: "Webmaster",
     lastUpdate: "17.04.2001",
     visitors: 1337
 };
-
 const links = [
     "index.html",
     "kuvat.html",
     "vieraskirja.html",
     "linkit.html"
 ];
-
 const browser = "Internet Explorer 5.5";
 const resolution = "800x600";
 
-
 const code = {};
-code.ABBGH10 = true;
-
-// ----------------------------------
-
+code.abbgh10 = true;
 console.log("Arkisto ladattu...");
 console.log("Version:", archive.version);
 
