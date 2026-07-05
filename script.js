596b0d06598c00a06307cf9c64bc453c4648501a
CITYALEKSI <turungraffitit@gmail.com>
2026-07-05 13:55:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+/*
+=====================================
+   KÄTKÖN NIMI
+   GC: GCBTB56
+   Arkistoversio 1.4
+=====================================
+*/
+
+// Arkiston tietoja
+const archive = {
+    version: "1.4",
+    author: "Webmaster",
+    lastUpdate: "17.04.2001",
+    visitors: 1337
+};
+
+// Muutama turha muuttuja
+const links = [
+    "index.html",
+    "kuvat.html",
+    "vieraskirja.html",
+    "linkit.html"
+];
+
+const browser = "Internet Explorer 5.5";
+const resolution = "800x600";
+
+// ----------------------------------
+// Jos löysit tänne asti,
+// olet oikeilla jäljillä.
+// ----------------------------------
+
+const code = {};
+code.ABBGH10 = true;
+
+// ----------------------------------
+
+console.log("Arkisto ladattu...");
+console.log("Version:", archive.version);
+
+// Vilkkuva otsikko selaimen välilehdessä
+const titles = [
+    "KÄTKÖN NIMI",
+    "*** TERVETULOA ***"
+];
+
+let i = 0;
+
+setInterval(() => {
+    document.title = titles[i];
+    i = (i + 1) % titles.length;
+}, 1500);
